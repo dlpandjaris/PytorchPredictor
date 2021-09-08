ba6f614dc747605cc324345e612c224d3668ba4a
dlpandjaris <dlpandjaris@gmail.com>
2021-09-07 22:58:59 -0500

    Updated initial_data_extraction and AMD_raw_data

--- initial_data_extraction.py
+++ initial_data_extraction.py
@@ -1,23 +1,31 @@
 import pandas_datareader as pdr
 from datetime import datetime
-import requests
 
 class Data_Getter:
     def __init__(self, ticker: str):
         self.ticker = ticker
-        raw_data = self.get_raw_data()
+        self.raw_data = self.get_raw_data()
+        self.reverse_data()
+        self.save_to_file()
 
-    def get_raw_data(self):
+    def get_raw_data(self) -> pdr.DataReader:
+        """Reads in raw data from yahoo finance"""
         start = datetime(2008, 9, 30)
-        end = datetime(2020, 1, 1)
+        end = datetime.now() #(2020, 1, 1)
         data = pdr.DataReader(self.ticker, 'yahoo', start, end)
         return(data)
-
+    
+    def reverse_data(self):
+        """Reverses row order in raw data"""
+        self.raw_data = self.raw_data.iloc[::-1]
+    
+    def save_to_file(self):
+        """Saves raw data to csv file"""
+        self.raw_data.to_csv("{}_raw_data.csv".format(self.ticker))
 
 def main():
-    Data_Getter("AMD")
-
+    data = Data_Getter("AMD")
+    print(data.raw_data)
 
 if __name__ == '__main__':
-    #main()
-    print(requests.get("https://www.yahoo.com"))
+    main()
