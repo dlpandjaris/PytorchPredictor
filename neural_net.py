f9a39201522f136ed2611c60e40afef2bd300c52
dlpandjaris <dlpandjaris@gmail.com>
2021-09-11 12:42:46 -0500

    made some changes to neural_net

--- neural_net.py
+++ neural_net.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import torch.optim as optim
 
 ticker: str = "AMD"
 data = pd.read_csv("{}_test_train_data.csv".format(ticker))
@@ -11,8 +12,6 @@ X = data.iloc[:,:-3]
 y = data["Profit-Bool"] #.iloc[:,-3:]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
 
 class Net(nn.Module):
     
@@ -34,6 +33,37 @@ class Net(nn.Module):
 net = Net()
 print(net)
 
-X = torch.Tensor(X.values)
-output = net(X)
-print(output)
+# X = torch.Tensor(X.values)
+# output = net(X)
+# print(output)
+
+optimiser = optim.Adam(net.parameters(), lr=0.001)
+
+EPOCHS = 3
+
+for epoch in range(EPOCHS):
+    for dataset in zip(X_train, y_train):
+        # dataset is a batch of featuresets and labels
+        X, y = dataset
+        net.zero_grad()
+        # Batch size helps generalisation
+        output = net(X.view())
+        loss = F.nll_loss(output, y)  # One hot vector, mean squared error    # Scalar vector use nll_loss
+        loss.backward()
+        optimiser.step()
+    print(loss)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
